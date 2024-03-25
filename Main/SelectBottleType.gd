db0d0b3a97f77d889791a2c87916b0cae4377c47
Ovidiu <ovycaprian@gmail.com>
2024-03-25 17:23:57 +0200

    Implemented functionality: when clicking outside the panel, it closes. Also, added comments above each function to provide context and explanation for their purpose.

--- Main/SelectBottleType.gd
+++ Main/SelectBottleType.gd
@@ -7,14 +7,6 @@ func _ready():
 
 func update_options():
 	add_item("330 ml", 0)
-	if GlobalVariables.bottleSize500:
-		add_item("500 ml", 1)
-	if GlobalVariables.bottleSize1000:
-		add_item("1000 ml", 2)
-	if GlobalVariables.bottleSize1500:
-		add_item("1500 ml", 3)
-	if GlobalVariables.bottleSize2000:
-		add_item("2000 ml", 4)
 
 func _on_item_selected(index):
 	match index:
